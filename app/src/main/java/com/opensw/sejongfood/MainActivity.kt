76eaafefa04a5d2d800e84f27a0651d82e9355b4
st4rlit <seonghwi@st4rlit.dev>
2024-06-04 15:45:05 +0900

    [Setting] ktlint 테스트

--- app/src/main/java/com/opensw/sejongfood/MainActivity.kt
+++ app/src/main/java/com/opensw/sejongfood/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val Abc = MainFragment() // lint test
     }
 }
