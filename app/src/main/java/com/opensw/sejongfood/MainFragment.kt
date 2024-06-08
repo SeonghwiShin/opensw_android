72dc6907668a5907976ab364182ce82fa2b08479
kimyeji521 <kyj447111@naver.com>
2024-06-08 20:50:51 +0900

    [Feat] 카카오 맵 동작 로직 추가

--- app/src/main/java/com/opensw/sejongfood/MainFragment.kt
+++ app/src/main/java/com/opensw/sejongfood/MainFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 import com.opensw.sejongfood.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +27,45 @@ class MainFragment : Fragment() {
             container,
             false,
         )
+
+        initView()
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.resume() // MapView 의 resume 호출
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.pause() // MapView 의 pause 호출
+    }
+    private fun initView() {
+        KakaoMapSdk.init(requireActivity(), "a75c80b77b641c9665023314aea7c763");
+
+        binding.mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+            }
+
+            override fun onMapError(error: Exception) {
+            }
+
+
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+            }
+            
+            override fun getPosition(): LatLng {
+                // 지도 시작 시 위치 좌표를 설정
+                return LatLng.from(37.406960, 127.115587)
+            }
+
+            override fun getZoomLevel(): Int {
+                // 지도 시작 시 확대/축소 줌 레벨 설정
+                return 15
+            }
+        })
     }
 }
