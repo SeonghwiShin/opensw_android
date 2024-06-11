5cb8ee513af7d0eebcaeea6dca83525e32ba04cc
kimdoh224 <kimdoh0224@gmail.com>
2024-06-12 04:44:22 +0900

    [Feat] 리뷰 리사이클러뷰 추가

---
+++ app/src/main/java/com/opensw/sejongfood/ReviewAdapter.kt
@@ -0,0 +1,41 @@
+package com.opensw.sejongfood
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.opensw.sejongfood.databinding.ItemReviewHolderBinding
+
+class ReviewAdapter(
+    private val dataList: List<Review>
+) : RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+
+    inner class ViewHolder(
+        private val binding: ItemReviewHolderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Review, position: Int) {
+            with(binding) {
+                ratingbar.rating = data.rating
+                textIndex.text = "${position + 1}번 유저"
+                textContent.text = data.contents
+                textRecommend.text = data.recommendMenu
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding = ItemReviewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        holder.bind(dataList[position], position)
+    }
+
+    override fun getItemCount() = dataList.size
+}
