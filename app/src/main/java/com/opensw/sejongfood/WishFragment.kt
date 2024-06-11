d0a3ec480b4a848438fc62c2b7f0dc243d50ec52
joyoze <joyongjun58@gmail.com>
2024-06-12 05:20:47 +0900

    [Feat] 더미데이터 삭제

--- app/src/main/java/com/opensw/sejongfood/WishFragment.kt
+++ app/src/main/java/com/opensw/sejongfood/WishFragment.kt
@@ -10,7 +10,7 @@ import com.opensw.sejongfood.databinding.FragmentWishBinding
 
 class WishFragment : Fragment() {
     private lateinit var binding: FragmentWishBinding
-
+    private var adapter: WishAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,35 +26,15 @@ class WishFragment : Fragment() {
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        adapter?.refreshData(SharedPreferenceUtil(requireActivity()).getPlaceDataList().toMutableList())
+    }
     private fun initView() {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
-        val dummyReviewList = mutableListOf(
-            Review(
-                rating = 4.5f,
-                contents = "와플이 정말 맛있어요! 커피도 좋아요.",
-                recommendMenu = "플레인와플, 아메리카노",
-            ),
-            Review(
-                rating = 4.0f,
-                contents = "친절한 직원과 깔끔한 매장.",
-                recommendMenu = "플레인와플, 아메리카노",
-            ),
-        )
-        val dataList = listOf(
-            PlaceData(
-                index = 1,
-                reviewCount = dummyReviewList.size,
-                review = dummyReviewList,
-                title = "와플대학 세종대학생회관캠퍼스",
-                rating = 4.0f,
-                latitude = 37.54972382209993,
-                longitude = 127.07525938973366,
-            ),
-        )
-
-        val adapter = WishAdapter(dataList)
+        adapter = WishAdapter(SharedPreferenceUtil(requireActivity()).getPlaceDataList().toMutableList())
         recyclerView.adapter = adapter
     }
 }
